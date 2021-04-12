12d02fcc99b9870c4475580e07d3847773c683b6
jose <jose@localhost>
2021-04-12 10:56:35 +0200

    Leer netflix

--- Ficheros/src/LeerNetflis.java
+++ Ficheros/src/LeerNetflis.java
@@ -1,43 +1,74 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import models.Show;
 
 public class LeerNetflis {
 
 	public static void main(String[] args) {
 
 		final String ruta = "assets/netflix_titles.csv";
+		var shows = leerFichero(ruta);
+		
+		//System.out.println(shows);
+		
+		shows.stream().filter(s -> s.getCountry().equalsIgnoreCase("spain")).forEach(System.out::println);
+
+	}
+	
+	public static List<Show> leerFichero(String ruta){
+
+		List<Show> misShows = new ArrayList<Show>();
 		File f = new File(ruta);
 		boolean isString = false;
 		Scanner sc = null;
 		try {
 			sc = new Scanner(f);
+
+			// Para quitar la cabecera (primera línea)
 			if (sc.hasNextLine())
 				sc.nextLine();
+
+			// Loh datoh
 			while (sc.hasNextLine()) {
 				var cadena = sc.nextLine();
 				var cadenitas = cadena.split(",");
+
 				String stringConComillas = "";
 				for (String s : cadenitas) {
-					//Para escapar cadenas completas de caracteres
-					if(s.startsWith("\"")) {
+					// Para escapar cadenas completas de caracteres
+					if (s.startsWith("\"")) {
 						isString = true;
 					}
-					if(isString) {
+					if (isString) {
 						stringConComillas += s + ",";
 					}
-					if(s.endsWith("\"")) {
-						isString = false;		
+					if (s.endsWith("\"")) {
+						isString = false;
 						s = stringConComillas;
 						stringConComillas = "";
-					}	
-					
-					//Imprimir el término entre las comas.
-					if(!isString) {
-						System.out.println(s.trim());
+					}
+
+					s = s.replaceAll("\"", "");
+					while (s.endsWith(",")) {
+						s = s.substring(0, s.length() - 1);
+					}
+					s = s.trim();
+
+					// Imprimir el término entre las comas.
+					if (!isString) {
+						// System.out.println(s);
 					}
 				}
-				System.out.println("---");
+				//if (cadenitas.length == 12) {
+					misShows.add(new Show(cadenitas[0], cadenitas[1], cadenitas[2], cadenitas[3], cadenitas[4],
+							cadenitas[5], cadenitas[6], cadenitas[7], cadenitas[8], cadenitas[9], cadenitas[10],
+							cadenitas[11]));
+
+				//}
 			}
 
 		} catch (FileNotFoundException e) {
@@ -45,7 +76,8 @@ public class LeerNetflis {
 		} finally {
 			sc.close();
 		}
-
+		
+		return misShows;
 	}
 
 }
--- Pilita/src/picassocollections/Pila.java
+++ Pilita/src/picassocollections/Pila.java
@@ -3,14 +3,20 @@ package picassocollections;
 import java.util.EmptyStackException;
 import java.util.LinkedList;
 
-public class Pila<J> {
-	private LinkedList<J> elementos;
+public class Pila<E> {
+	private LinkedList<E> elementos;
 
 	public Pila() {
-		elementos = new LinkedList<J>();
+		elementos = new LinkedList<E>();
 	}
 
-	public void add(J elemento) {
+	/**
+	 * Añade un elemento a la cima de la pila
+	 * 
+	 * @param elemento a añadir
+	 * @throws NullPointerException no permite añadir valores nulos.
+	 */
+	public void push(E elemento) throws NullPointerException {
 		if (elemento != null) {
 			elementos.add(elemento);
 		} else {
@@ -18,25 +24,44 @@ public class Pila<J> {
 		}
 	}
 
-	public J peek() {
+	/**
+	 * Extrae el elemento en la cima de la pila si hay alguno.
+	 * 
+	 * @return elemento E de la cima
+	 * @throws EmptyStackException en caso de estar vacía
+	 */
+	public E pop() throws EmptyStackException {
 		if (!elementos.isEmpty())
 			return elementos.pollLast();
 		else
 			throw new EmptyStackException();
 	}
 
-	public void clear() {
-		elementos.clear();
-	}
-	
-	@Override
-	public String toString() {
-		String out = "--\n";
-		for (int i = elementos.size() -1 ; i >= 0 ; i-- ) {
-			out += elementos.get(i) + "\n";
-		} 
-		out += "--";
-		return out;
+	/**
+	 * Determina si la pila está vacía o no.
+	 * 
+	 * @return true si la pila está vacía.
+	 */
+	public boolean isEmpty() {
+		return elementos.isEmpty();
 	}
 
+	/**
+	 * Devuelve el tamaño de la pila
+	 * 
+	 * @return tamaño de la pila.
+	 */
+	public int size() {
+		return elementos.size();
+	}
+
+	/**
+	 * Determina si un objecto está en la pila o no.
+	 * 
+	 * @param object objeto a comprobar.
+	 * @return true si el objeto ya está en la pila
+	 */
+	public boolean contains(E object) {
+		return elementos.contains(object);
+	}
 }
