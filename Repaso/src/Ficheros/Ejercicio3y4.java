2b4fd4a422c6d70751b545a75f7410e2d72f941f
jose <jose@localhost>
2021-04-16 09:11:14 +0200

    Inicio ficheros 3 y 4

---
+++ Repaso/src/Ficheros/Ejercicio3y4.java
@@ -0,0 +1,13 @@
+package Ficheros;
+
+public class Ejercicio3y4 {
+
+	public void escribirEnFichero(String[] cadenas, String filename) {
+
+	}
+
+	public void leerEnFichero(String filename) {
+
+	}
+
+}
